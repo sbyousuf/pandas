2c358593246b8d66ac981d6fa2f02cc4b0b973e3
saba yousefzadeh <sbyousef83@gmail.com>
2024-07-20 16:06:25 +0430

    fixing

--- main.py
+++ main.py
@@ -3,14 +3,14 @@
 from pandas_project.sort_function import sort
 
 # Define patterns for hashtags, mentions, and dollar words
-hashtag_pattern = r'#(\w+)'
-mention_pattern = r'@(\w+)'
-dollar_pattern = r'\$(\w+)'
+hashtag_pattern = r"#(\w+)"
+mention_pattern = r"@(\w+)"
+dollar_pattern = r"\$(\w+)"
 
 # Input CSV file path
-input_file = 'data/django_crypto_twitter_influencers_tweet.csv'
+input_file = "data/django_crypto_twitter_influencers_tweet.csv"
 
 # Sort and save data for hashtags, mentions, and dollar words
-sort(hashtag_pattern, input_file, 'sorted_hashtags.csv')
-sort(mention_pattern, input_file, 'sorted_mentions.csv')
-sort(dollar_pattern, input_file, 'sorted_dollars.csv')
+sort(hashtag_pattern, input_file, "sorted_hashtags.csv")
+sort(mention_pattern, input_file, "sorted_mentions.csv")
+sort(dollar_pattern, input_file, "sorted_dollars.csv")
--- pandas_project/sort_function.py
+++ pandas_project/sort_function.py
@@ -4,17 +4,20 @@ import pandas as pd
 import re
 from collections import Counter
 
+
 def sort(pattern, input_file, output_file):
     df = pd.read_csv(input_file, low_memory=False)
 
     def find_words(text):
         return re.findall(pattern, str(text))
-    
-    words = df['full_text'].apply(find_words).explode().dropna().tolist()
+
+    words = df["full_text"].apply(find_words).explode().dropna().tolist()
     counter = Counter(words)
     sorted_list = sorted(counter.items(), key=lambda x: x[1], reverse=True)
-    indexed_list = [(index + 1, word, count) for index, (word, count) in enumerate(sorted_list)]
-    sorted_df = pd.DataFrame(indexed_list, columns=['Index', 'Name', 'Count'])
+    indexed_list = [
+        (index + 1, word, count) for index, (word, count) in enumerate(sorted_list)
+    ]
+    sorted_df = pd.DataFrame(indexed_list, columns=["Index", "Name", "Count"])
     sorted_df.to_csv(output_file, index=False)
 
     print(f"The sorted data has been saved to '{output_file}'")
